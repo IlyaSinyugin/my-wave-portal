3c8c2864a23d02800e44082f0caa9997f36dc691
Ilya <ilya@sinyugin.ru>
2022-04-29 08:54:06 +0200

    contract deployment

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,1 +1,30 @@
 // file for server deployments
+
+const { hexStripZeros } = require("ethers/lib/utils")
+
+// write "npx hardhat node" to start a local Ethereum network 
+const main = async () => {
+    const [deployer] = await hre.ethers.getSigners();
+    const accountBalance = await deployer.getBalance();
+
+    console.log("Deploying contracts with account: ", deployer.address);
+    console.log("Account balance: ", accountBalance.toString());
+
+    const waveContractFactory = await hre.ethers.getContractFactory("WavePortal");
+    const waveContract = await waveContractFactory.deploy();
+    await waveContract.deployed();
+
+    console.log("WavePortal address: ", waveContract.address);
+}
+
+const runMain = async () => {
+    try {
+        await main();
+        process.exit(0);
+    } catch (error) {
+        console.log(error);
+        process.exit(1);
+    }
+}
+
+runMain();
