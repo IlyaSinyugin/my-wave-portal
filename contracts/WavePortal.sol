63ea14497bb34bb1526761faa3de6f435370baed
Ilya <ilya@sinyugin.ru>
2022-05-01 16:49:59 +0200

    updated contract

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -8,14 +8,37 @@ import "hardhat/console.sol";
 contract WavePortal {
     uint256 totalWaves;
 
+    // added event
+    event NewWave(address indexed from, uint256 timestamp, string message);
+
+    // created structure aka custom datatype
+    struct Wave {
+        address waver;
+        string message;
+        uint256 timestamp;
+    }
+
+    // storing array of structs
+    Wave[] waves;
+
     constructor() {
         console.log("I am a smart contract. Hello World!");
     }
 
-    function wave() public {
+    function wave(string memory _message) public {
         totalWaves += 1;
-        console.log("%s has waved!", msg.sender);
+        console.log("%s has waved!", msg.sender, _message);
         // msg.sender - wallet address of the person who called the function
+
+        // store data in array
+        waves.push(Wave(msg.sender, _message, block.timestamp));
+
+        emit NewWave(msg.sender, block.timestamp, _message);
+    }
+
+    // return struct array waves
+    function getAllWaves() public view returns (Wave[] memory) {
+        return waves;
     }
 
     // view function - doesn't alter the state variable but only reads them
--- scripts/run.js
+++ scripts/run.js
@@ -1,40 +1,35 @@
 const main = async () => {
-    const [owner, randomPerson] = await hre.ethers.getSigners();
-    const waveContractFactory = await hre.ethers.getContractFactory("WavePortal");
-    const waveContract = await waveContractFactory.deploy();
-    await waveContract.deployed();
-    console.log("Contract deployed to: ", waveContract.address);
+  const waveContractFactory = await hre.ethers.getContractFactory("WavePortal");
+  const waveContract = await waveContractFactory.deploy();
+  await waveContract.deployed();
+  console.log("Contract addy:", waveContract.address);
 
-    console.log("Contract deployed to: ", waveContract.address);
-    console.log("Contract deployed by: ", owner.address);
-    const signers = await hre.ethers.getSigners();
-    console.log("All signers: ");
-    for (const signer of signers) {
-        console.log(signer.address);
-    }
+  let waveCount;
+  waveCount = await waveContract.getTotalWaves();
+  console.log(waveCount.toNumber());
 
-    let waveCount;
-    waveCount = await waveContract.getTotalWaves();
+  /**
+   * Let's send a few waves!
+   */
+  let waveTxn = await waveContract.wave("A message!");
+  await waveTxn.wait(); // Wait for the transaction to be mined
 
-    let waveTxn = await waveContract.wave();
-    await waveTxn.wait();
+  const [_, randomPerson] = await hre.ethers.getSigners();
+  waveTxn = await waveContract.connect(randomPerson).wave("Another message!");
+  await waveTxn.wait(); // Wait for the transaction to be mined
 
-    waveCount = await waveContract.getTotalWaves();
-
-    waveTxn = await waveContract.connect(randomPerson).wave();
-    await waveTxn.wait();
-
-    waveCount = await waveContract.getTotalWaves();
-
+  let allWaves = await waveContract.getAllWaves();
+  console.log(allWaves);
 };
 
 const runMain = async () => {
-    try {
-        await main(); 
-        process.exit(0); //exit Node process without error
-    } catch (error) {
-        console.log(error);
-        process.exit(1); // exit Node process while indicating "Uncaught Fatal Exception" error
-    }
+  try {
+    await main();
+    process.exit(0);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
 };
+
 runMain();
